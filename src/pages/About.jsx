ad04dcf7f72a8db431ac24a501a3416d0e91473d
darren <darren@holmgraphics.ca>
2026-01-17 19:03:59 +0000

    File changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -143,11 +143,11 @@ Holm Potato Farms grows approimately 800 acres potatoes annually, focussed on th
                             className="w-full h-48 object-cover rounded-xl" />
 
                                 <img
-                            src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/696b7ab40d412f960295a323/7ab4b7466_Planter2.jpg"
-                            alt="Farm image"
-                            className="w-full h-64 object-cover rounded-xl" />
+                                src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/696b7ab40d412f960295a323/d82a12661_Sprayer1.jpg"
+                                alt="Farm image"
+                                className="w-full h-64 object-cover rounded-xl" />
 
-                             </div>
+                                </div>
                              <div className="space-y-4 pt-8">
                                 <img
                             src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/696b7ab40d412f960295a323/7ab4b7466_Planter2.jpg"
