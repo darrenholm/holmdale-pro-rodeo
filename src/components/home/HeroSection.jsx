f5eff69bb26924a27963cda301fc1ae984e7b27c
darren <darren@holmgraphics.ca>
2026-01-18 14:06:18 +0000

    Manual code change

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -54,7 +54,7 @@ export default function HeroSection({ featuredEvent }) {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.1 }}>
 
-                    <span className="mt-20 block">Holmdale</span>
+                    <span className="mt-32 block">Holmdale</span>
                     <span className="text-green-500 mx-auto my-5">Pro Rodeo
 
           </span>
