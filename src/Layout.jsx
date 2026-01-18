12f48a91420b5696d7f3a244a20ea20499314275
darren <darren@holmgraphics.ca>
2026-01-18 13:52:57 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './components/shared/Navbar';
 import Footer from './components/shared/Footer';
+import SponsorTicker from './components/SponsorTicker';
 
 export default function Layout({ children }) {
     return (
         <div className="min-h-screen bg-stone-950">
             <Navbar />
+            <SponsorTicker />
             <main>
                 {children}
             </main>
