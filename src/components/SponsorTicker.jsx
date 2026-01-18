83321b2374d54668bb56ebc9ad2d5b4b1950d45b
darren <darren@holmgraphics.ca>
2026-01-18 13:54:10 +0000

    File changes

--- src/components/SponsorTicker.jsx
+++ src/components/SponsorTicker.jsx
@@ -10,11 +10,6 @@ const sponsors = [
 export default function SponsorTicker() {
   return (
     <div className="bg-white border-t border-stone-200 py-8 overflow-hidden">
-      <div className="mb-6 text-center">
-        <h3 className="text-stone-600 text-sm font-semibold tracking-wider uppercase">
-          Proud Partners & Sponsors
-        </h3>
-      </div>
       
       <div className="relative">
         <div className="flex">
