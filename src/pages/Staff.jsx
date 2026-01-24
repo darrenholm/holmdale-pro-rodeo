16e7450b32b9d2a92515e8103354ba715c279bce
darren <darren@holmgraphics.ca>
2026-01-24 16:07:44 +0000

    File changes

--- src/pages/Staff.jsx
+++ src/pages/Staff.jsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { QrCode, Ticket, CreditCard, LogOut } from 'lucide-react';
-import { createPageUrl } from 'utils';
+import { createPageUrl } from '@/utils';
 
 const STAFF_PASSWORD = 'staff2026'; // Change this to your desired password
 
