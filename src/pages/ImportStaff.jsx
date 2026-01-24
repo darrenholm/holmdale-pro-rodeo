8d1d0b8d7e61d747c132c873e0974def9f70307f
darren <darren@holmgraphics.ca>
2026-01-24 20:10:46 +0000

    File changes

--- src/pages/ImportStaff.jsx
+++ src/pages/ImportStaff.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { base44 } from '@/api/base44Client';
-import { createPageUrl } from './utils';
+import { createPageUrl } from '@/utils';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Upload, CheckCircle, AlertCircle, Database } from 'lucide-react';
