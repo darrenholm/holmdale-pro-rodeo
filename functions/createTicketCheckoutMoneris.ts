35f4ffb9306da7ca4c023a46d3bc6f8f02cd27a6
darren <darren@holmgraphics.ca>
2026-01-29 19:56:14 +0000

    File changes

--- functions/createTicketCheckoutMoneris.ts
+++ functions/createTicketCheckoutMoneris.ts
@@ -52,6 +52,7 @@ Deno.serve(async (req) => {
     };
 
     console.log('Creating Moneris Checkout for:', orderId);
+    console.log('Moneris request data:', JSON.stringify(checkoutData, null, 2));
     const monerisResponse = await fetch('https://gateway.moneris.com/chktv2/request/request.php', {
       method: 'POST',
       headers: {
