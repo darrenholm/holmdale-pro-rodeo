4b00f864c4ff783f171975d9525e3009a8aed181
darren <darren@holmgraphics.ca>
2026-01-31 22:46:43 +0000

    File changes

--- src/pages/LinkRFID.jsx
+++ src/pages/LinkRFID.jsx
@@ -35,6 +35,10 @@ export default function LinkRFID() {
   const rfidInputRef = useRef(null);
 
   useEffect(() => {
+    setFormLoadTime(new Date());
+  }, []);
+
+  useEffect(() => {
     if (scanning && step === STEP.SCAN_QR) {
       startCamera();
     } else {
